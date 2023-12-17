d637590726e233d720d858e274a3c05d85a1581b
Konstantin <kkohno@mail.ru>
2023-12-18 01:40:47 +0700

    Update room.js

--- client/room/scripts/room.js
+++ client/room/scripts/room.js
@@ -32,7 +32,7 @@ if (blue || !red && !blue) {
 // разрешаем вход в команды по запросу
 Teams.OnRequestJoinTeam.add_Event(function (player, team) { team.Add(player); });
 // спавн по входу в команду
-Teams.OnPlayerChangeTeam.add_Event(function (player) {
+Teams.OnPlayerChangeTeam.add_Event(function (player, teams) {
 	player.Spawns.Spawn();
 });
 
